929a44ae84b5ff47d137e28c33ee7686260784ca
SerhiySBV <serhiybelyavskii@gmail.com>
2023-01-07 22:31:39 +0200

    created user card

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
+import userData from 'data/user.json';
+import { Profile } from './Profile/Profile';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        followers={userData.stats.followers}
+        views={userData.stats.views}
+        likes={userData.stats.likes}
+      />
     </div>
   );
 };
